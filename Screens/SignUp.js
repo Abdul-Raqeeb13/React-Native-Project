0497cb77c13442535e4521ded811503ef9be898b
Abdul-Raqeeb13 <abdulraqeeb97568@gmail.com>
2024-08-08 03:06:09 +0500

    Firebase Authentication

--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -1,201 +1,284 @@
 import React, { useState } from 'react';
-import { View, Image, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
-import { Modal, Portal, Text, Provider as PaperProvider, TextInput } from 'react-native-paper';
+import { View, Image, StyleSheet, SafeAreaView, TouchableOpacity, ScrollView } from 'react-native';
+import { Modal, Portal, Text, Provider as PaperProvider, TextInput, Button } from 'react-native-paper';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
+import storage from '@react-native-firebase/storage';
 
 export default function SignUp() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [image, setImage] = useState("");
 
-  // for moadl open and clse control
   const [visible, setVisible] = useState(false);
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
 
-  // for camera control
-  const [file , setfile] = useState("")
+  const [file, setfile] = useState("");
 
   const openCamera = () => {
-    setVisible(false)
+    setVisible(false);
     const option = {
       mediaType: 'photo',
       quality: 0.8
-  }
+    };
 
-  launchCamera(option, (response) => {
-    if (response.didCancel) {
+    launchCamera(option, (response) => {
+      if (response.didCancel) {
         console.log('User cancelled the camera.');
-    }
-    else if (response.error) {
+      } else if (response.error) {
         console.log('Camera Error: ', response.error);
-    }
-    else {
-        // Use the image from the camera here
+      } else {
         console.log('Image URI camera: ', response.assets[0].uri);
-        setfile(response.assets[0].uri)
-       
-
-    }
-
-})
-  } 
+        setImage(response.assets[0].uri);
+      }
+    });
+  };
 
   const openImageGallery = () => {
-    setVisible(false)
+    setVisible(false);
     const option = {
       mediaType: 'photo',
       quality: 0.8
-  }
-
- 
+    };
 
     launchImageLibrary(option, (response) => {
       if (response.didCancel) {
-          console.log('User cancelled the camera.');
+        console.log('User cancelled the camera.');
+      } else if (response.error) {
+        console.log('Camera Error: ', response.error);
+      } else {
+        console.log('Image URI: ', response.assets[0].uri);
+        setImage(response.assets[0].uri);
       }
-      else if (response.error) {
-          console.log('Camera Error: ', response.error);
-      }
-      else {
-          // Use the image from the camera here
-          console.log('Image URI: ', response.assets[0].uri);
-          setfile(response.assets[0].uri)
-         
+    });
+  };
 
-      }
+  const uploadImage = async () => {
+    const reference = storage().ref('User Profile');
+    await reference.putFile(image);
+    const url = await reference.getDownloadURL();
 
-  })
+    console.log("Download URL", url);
+    return url;
+  };
 
-}
-  
+  const signUpuser = () => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+    if (name === undefined) {
+      console.warn("Name must be required");
+    } else if (email === undefined) {
+      console.warn("Email must be required");
+    } else if (!regex.test(email)) {
+      console.warn("Enter valid email");
+    } else if (password === undefined) {
+      console.warn("Password must be required");
+    } else {
+      auth().createUserWithEmailAndPassword(email, password)
+        .then(async (UserCredential) => {
+          var ProFileLink = await uploadImage();
+
+          var userData = {
+            UserName: name,
+            UserEmail: email,
+            UserPassword: password,
+            UserProfile: ProFileLink,
+            UserID: UserCredential.user.uid
+          };
+
+          const reference = database().ref(`Users/${UserCredential.user.uid}`);
+          await reference.set(userData);
+
+          console.warn('User account created & signed in!');
+          setName("");
+          setEmail("");
+          setPassword("");
+        })
+        .catch(error => {
+          if (error.code === 'auth/email-already-in-use') {
+            console.warn('That email address is already in use!');
+          }
+
+          if (error.code === 'auth/invalid-email') {
+            console.warn('That email address is invalid!');
+          }
+
+          console.error(error);
+        });
+    }
+  };
 
   return (
-    <PaperProvider>
-      <SafeAreaView style={styles.container}>
-        <Image source={require("../Assets/layer3.png")} style={styles.image} />
-        <View style={styles.overlay}>
-          <Text style={styles.title}>SignUp</Text>
-        </View>
+    <>
+      <PaperProvider>
+        <SafeAreaView style={styles.container}>
+          <ScrollView contentContainerStyle={styles.scrollViewContent}>
+            <Image source={require("../Assets/BgCover.png")} style={styles.image} />
+            <View style={styles.overlay}>
+              <Text style={styles.title}>SIGN UP</Text>
+            </View>
 
-        <View style={styles.form}>
-          <TextInput style={styles.inp} mode='outlined' label="Name" />
-          <TextInput style={styles.inp} mode='outlined' label="Email" />
-          <TextInput style={styles.inp} mode='outlined' label="Password" />
-          <Text style={styles.text}>Upload Image</Text>
-          <TouchableOpacity style={styles.imgCon} onPress={showModal}>
-            <FeatherIcon name="image" size={30} color="black" />
-          </TouchableOpacity>
+            <View style={styles.form}>
+              <TextInput style={styles.inp} mode='outlined' label="Name" value={name} onChangeText={(e) => { setName(e) }} />
+              <TextInput style={styles.inp} mode='outlined' label="Email" value={email} onChangeText={(e) => { setEmail(e) }} />
+              <TextInput style={styles.inp} mode='outlined' label="Password" value={password} onChangeText={(e) => { setPassword(e) }} />
+              <Text style={styles.text}>Upload Image</Text>
+              <TouchableOpacity style={styles.imgView} onPress={showModal}>
+                {
+                  image === "" ?
+                    <TouchableOpacity style={styles.imgCon} onPress={showModal}>
+                      <FeatherIcon name="image" size={30} color="black" />
+                    </TouchableOpacity> :
+                    <Image
+                      style={styles.Image1}
+                      source={{ uri: image }}
+                      resizeMode={"cover"}
+                    />
+                }
+              </TouchableOpacity>
 
+              <TouchableOpacity onPress={() => signUpuser()} style={styles.signupBtn}>
+                <Text style={styles.signupBtnText}>Sign Up</Text>
+              </TouchableOpacity>
+              
+              <TouchableOpacity style={styles.signinBtn}>
+                <Text style={styles.signinBtnText}>Sign In</Text>
+              </TouchableOpacity>
 
-        {/* react native paper modal */}
-          <Portal>
-            <Modal
-              visible={visible}
-              onDismiss={hideModal}
-              contentContainerStyle={styles.modalContainer}
-            >
-              <View style={styles.selectIamgeModelContainer}>
+              <Portal>
+                <Modal
+                  visible={visible}
+                  onDismiss={hideModal}
+                  contentContainerStyle={styles.modalContainer}
+                >
+                  <View style={styles.selectImageModelContainer}>
+                    <TouchableOpacity style={styles.selectModeIcon} onPress={() => openImageGallery()}>
+                      <MaterialIcon name="image" size={30} color="black" />
+                      <Text>Gallery</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity style={styles.selectModeIcon} onPress={()=>openImageGallery()}>
-                  <MaterialIcon name="image" size={30} color="black" />
-                  <Text>Gallery</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.selectModeIcon} onPress={()=>openCamera()}>
-                  <MaterialIcon name="camera" size={30} color="black" />
-                  <Text>Camera</Text>
-                </TouchableOpacity>
-                
-              </View>
-            </Modal>
-          </Portal>
-        </View>
-      </SafeAreaView>
-    </PaperProvider>
+                    <TouchableOpacity style={styles.selectModeIcon} onPress={() => openCamera()}>
+                      <MaterialIcon name="camera" size={30} color="black" />
+                      <Text>Camera</Text>
+                    </TouchableOpacity>
+                  </View>
+                </Modal>
+              </Portal>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </PaperProvider>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-
-
-  // main container
   container: {
     flex: 1,
     backgroundColor: "white",
-    // backgroundColor
   },
-
-  // background curver image
+  scrollViewContent: {
+    // flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   image: {
     width: '100%',
-    height: '35%',
+    height: 200,
     resizeMode: 'cover',
   },
-
-  // text over background image
   overlay: {
-    flex: 1,
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
-    bottom: 0,
     alignItems: 'center',
     marginTop: 30,
   },
-
-  // text style over background image
   title: {
     color: 'white',
     fontSize: 50,
     marginTop: 20,
   },
-
-  // form container 
   form: {
-    margin: 10,
+    width: '90%',
+    marginTop: -10,
   },
-
-  // input spacing
   inp: {
-    margin: 10,
+    marginVertical: 10,
   },
-
-  // from text styling
   text: {
     marginLeft: 10,
     marginTop: 10,
   },
-
-  // image icon adjust
   imgCon: {
     alignItems: "center",
-    marginTop: -20,
+    marginTop: -50,
   },
-
-
-  /* react native paper modal styling*/
-
   modalContainer: {
     backgroundColor: 'white',
-    width: '100%',
-    padding: 20,
+    padding: 10,
     position: 'absolute',
     bottom: 0,
+    width: '100%',
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
   },
-
-
-  selectIamgeModelContainer: {
-    display: "flex",
-    flexDirection: "row"
+  selectImageModelContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
-
   selectModeIcon: {
     alignItems: "center",
-    marginRight: 20
-    // marginTop: -20,
   },
+  imgView: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  Image1: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+
+  signupBtn : {
+    elevation: 8,
+    backgroundColor: "#663399",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12
+  },
+  signupBtnText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  },
+
+  signinBtn: {
+    elevation: 8,
+    backgroundColor: "white",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    marginVertical : 10,
+    borderColor:"#663399",
+    borderWidth:1
+  },
+
+  signinBtnText: {
+    fontSize: 18,
+    color: "#663399",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase",
+   
+  }
 });
